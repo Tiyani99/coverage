252470cbf1554444912adaf123778c71fb6b08cf
Tiyani99 <Tiyani.angel1999@gmail.com>
2020-07-31 14:46:49 +0200

    first commit

---
+++ src/test/index.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+const { expect } = chai;
+
+describe('Initial test', () => {
+  it('should be equal to generic thing', () => {
+    expect('this thing').to.equal('this thing');
+  });
+});
