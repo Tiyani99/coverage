20f451d634072b53a9a298fcb9b8ad09863e467f
Tiyani99 <59081555+Tiyani99@users.noreply.github.com>
2020-08-10 15:49:26 +0200

    Delete index.js

--- src/test/index.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-
-const { expect } = chai;
-
-describe('Initial test', () => {
-  it('should be equal to generic thing', () => {
-    expect('this thing').to.equal('this thing');
-  });
-});
